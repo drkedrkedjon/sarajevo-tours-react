828d49b7683e32858131ea7c79ede43bbd44a3b7
Sasa Savic Perusina <sasa@bubulazy.com>
2023-03-26 17:50:35 +0100

    Head component CSS work, hero-select finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <div className="hero-container">
+      <div className="hero-grid">
+        <div className="hero-fondo"></div>
+        <div className="hero-text">
+          <h2>Best adventure gps guides in and around the city of:</h2>
+          <h2>Sarajevo</h2>
+        </div>
+        <div className="hero-select">
+          <h3 className="color-white">Choose your passion:</h3>
+          <div className="hero-btn-container">
+            <button className="btn">Skiing</button>
+            <button className="btn">Biking</button>
+            <button className="btn">Walking</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
