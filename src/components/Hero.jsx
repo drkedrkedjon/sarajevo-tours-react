c5406bc0b1ccbfcffba06060d0502aace55d7f57
Sasa Savic Perusina <sasa@bubulazy.com>
2023-03-27 13:35:22 +0100

    Finishing CSS of hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,14 @@ export default function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-grid">
-        <div className="hero-fondo"></div>
+        <div className="hero-plate"></div>
         <div className="hero-text">
-          <h2>Best adventure gps guides in and around the city of:</h2>
-          <h2>Sarajevo</h2>
+          <h2 className="ff-title color-white text-stroke">
+            Best adventure gps guides in and around the city of:
+          </h2>
+          <h2 className="ff-title color-blue text-stroke hero-text-title">
+            Sarajevo
+          </h2>
         </div>
         <div className="hero-select">
           <h3 className="color-white">Choose your passion:</h3>
