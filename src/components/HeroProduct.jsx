d3a3e45448cf8f1c0c264e662b31ab702aeade06
Sasa Savic Perusina <sasa@bubulazy.com>
2023-04-02 13:18:24 +0100

    Working on HeroProduct component. Passing props from product

---
+++ src/components/HeroProduct.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="hero-grid">
+        <div className="hero-plate"></div>
+        <div className="hero-text">
+          <h2 className="ff-title color-white text-stroke">
+            Best adventure gps guides in and around the city of:
+          </h2>
+          <h2 className="ff-title color-blue text-stroke hero-text-title">
+            Sarajevo
+          </h2>
+        </div>
+        <div className="hero-select">
+          <h3 className="color-white fs-500">Choose your passion:</h3>
+          <div className="hero-btn-container">
+            <NavLink
+              to="skiing"
+              className={({ isActive }) =>
+                isActive ? "btn-active btn" : "btn btn-green"
+              }
+            >
+              Skiing
+            </NavLink>
+            <NavLink
+              to="biking"
+              className={({ isActive }) =>
+                isActive ? "btn-active btn" : "btn btn-green"
+              }
+            >
+              Biking
+            </NavLink>
+            <NavLink
+              to="walking"
+              className={({ isActive }) =>
+                isActive ? "btn-active btn" : "btn btn-green"
+              }
+            >
+              Walking
+            </NavLink>
+          </div>
+          <NavLink className="underline" to="/">
+            Clear selection
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// className={`${({ isActive }) =>
+//   isActive ? "btn-active" : ""} btn btn-green`}
