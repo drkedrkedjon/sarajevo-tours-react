212e7500e750707f37413477182a67c9800306b1
Sasa Savic Perusina <sasa@bubulazy.com>
2023-04-16 16:50:52 +0100

    Working on shopping cart list

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,44 @@
 import React, { useContext } from "react";
 import { ShoppingContext } from "../api/ShopCartContext";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Cart() {
   const [shopCart, setShopCart] = useContext(ShoppingContext);
   console.log(shopCart);
-  return <h1>Cart</h1>;
+  return (
+    <div className="background-color-home">
+      <div className="home-layout home-layout-blue">
+        <Header color="color-orange" />
+        <div className="cart-container">
+          <h1 className="ff-title color-black fs-700">Shopping cart</h1>
+          <p>Items in your cart:</p>
+          <ol className="cart-list">
+            <li className="cart-list-item">
+              <div className="title-summary">
+                <h3>Title</h3>
+                <p>Summary</p>
+              </div>
+              <p className="cart-price">25$</p>
+            </li>
+            <li className="cart-list-item">
+              <div className="title-summary">
+                <h3>Title</h3>
+                <p>Summary</p>
+              </div>
+              <p className="cart-price">25$</p>
+            </li>
+            <li className="cart-list-item">
+              <div className="title-summary">
+                <h3>Title</h3>
+                <p>Summary</p>
+              </div>
+              <p className="cart-price">25$</p>
+            </li>
+          </ol>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
